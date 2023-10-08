073d019260f117a5c9cbe23cdec2796d9704dee9
daniele-rizzi <133057546+daniele-rizzi@users.noreply.github.com>
2023-10-08 18:04:10 +0200

    First Project Commit

---
+++ Gym/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Gym
+//
+//  Created by Deborah Nimis on 10/3/23.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView(.horizontal){
+            LazyHStack(){
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.blue, lineWidth: 5)
+                        .frame(width: 150, height: 150)
+                        .tag(0)
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.blue, lineWidth: 5)
+                        .frame(width: 150, height: 150)
+                        .tag(1)
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.blue, lineWidth: 5)
+                        .frame(width: 150, height: 150)
+                        .tag(2)
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.blue, lineWidth: 5)
+                        .frame(width: 150, height: 150)
+                        .tag(3)
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(.blue, lineWidth: 5)
+                        .frame(width: 150, height: 150)
+                        .tag(4)
+            }
+            .padding([.horizontal])
+        }
+        .scrollTargetBehavior(.paging)
+        HStack{
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+                
+        }
+        
+        HStack{
+            RoundedRectangle(cornerRadius:25)
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+                .padding([.top])
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        
+}
+
---
+++ Gym/GymApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymApp.swift
+//  Gym
+//
+//  Created by Deborah Nimis on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
